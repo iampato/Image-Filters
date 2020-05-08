b896e55a51f130e34dcd559e44641371ca278dde
Iampato <858wpwaweru@gmail.com>
2020-05-08 09:31:09 +0300

    ready for beta release

--- lib/filter.dart
+++ lib/filter.dart
@@ -7,10 +7,6 @@ class Filter {
   static const MethodChannel _channel =
       const MethodChannel('filter');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
   static Future getThumbs(Uint8List data) async {
     var result = await _channel.invokeMethod('generateFilters', data);
     return result;
